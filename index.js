967686b4b3d957ebc6db3f3432c1f405350ae712
Gazmir Gaz <gazmir1000@gmail.com>
2022-05-01 18:51:08 +0200

    a lot of changes

--- index.js
+++ index.js
@@ -7,3 +7,53 @@ for (let i = 0; i < scrollButtons.length; i++) {
     window.scrollBy(0, pageHeight);
   });
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+
+function initMap() {
+  // ktu eshte fshnjaaaaa
+  const fshn = { lat: 41.334458, lng: 19.816468 };
+  // The map, centered at fshnn
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: fshn,
+  });
+  // The marker, positioned at fshn
+  const marker = new google.maps.Marker({
+    position: fshn,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
